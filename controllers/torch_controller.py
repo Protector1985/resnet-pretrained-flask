5659ad0d6633c056ab7091df14bf33c35e8944c1
Protector1985 <michaelrbraun1985@gmail.com>
2024-01-10 08:59:19 -0800

    added the new class and image script

--- controllers/torch_controller.py
+++ controllers/torch_controller.py
@@ -1,11 +1,18 @@
+from flask import request
+from services.torch_service import Torch_Service
+from PIL import Image
+import io
 
-
-class Torch_Controller:
+class Torch_Controller(Torch_Service):
     basePath="/torch"
-    
         
     def initRoutes(self, app):
-        print(self.basePath)
         @app.route(self.basePath + "/data", methods=["POST"])
         def handleRequest():
-            print("Request came in!")
+            if request.method == "POST":
+                if 'image' in request.files:
+                    image_file = request.files['image']
+                    image = Image.open(io.BytesIO(image_file.read()))
+                    image.show()
+                    
+                return "OK"
---
+++ services/torch_service.py
@@ -0,0 +1,5 @@
+from torchvision import models, transforms
+
+class Torch_Service:
+    def ai_inference_magic(self, data):
+        print(data)
